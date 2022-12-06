23463a424238b4d704fbb22b3a75be45e83c08fb
Thien Trieu <thien.trieu@telus.com>
2022-12-06 15:59:14 -0800

    implement renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,35 +4,98 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
+// const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//     "handle": "@SirIsaac"
+//   },
+//   "content": {
+//     "text": "If I have seen further it is by standing on the shoulders of giants"
+//   },
+//   "created_at": 1461116232227
+// };
 
-const createTweetElement = function(object) {
-  
-  const markup = `
+// const createTweetElement = function(object) {
+
+//   let markup = `
+//   <article class="tweets">
+//   <header class="tweetHeader">
+//     <div class="tweeterProfile">
+//       <img class="avatar" src="${object.user.avatars}">
+//       <h5 class="tweeterName">${object.user.name}</h5>
+//     </div>
+//     <div class="tweetUser">
+//       <h4 class="userName">${object.user.handle}</h4>
+//     </div>
+//   </header>
+//     <article class="tweetText">${object.content.text}</article>
+//   <hr>
+//   <footer class="tweetFooter">
+//     <h6>${object.created_at}</h6>
+//     <div>
+//       <i class="fa-solid fa-flag icons"></i>
+//       <i class="fa-sharp fa-solid fa-retweet icons"></i>
+//       <i class="fa-solid fa-heart icons"></i>
+//     </div>
+//   </footer>
+// </article>`;
+
+//   return markup;
+// };
+
+// const $tweet = createTweetElement(tweetData);
+
+
+
+// $(document).ready(function() {
+//   $('#tweets-container').append($tweet);
+// });
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+
+
+const createTweetElement = function(tweet) {
+  let $tweet = `
   <article class="tweets">
   <header class="tweetHeader">
     <div class="tweeterProfile">
-      <img class="avatar" src="${object.user.avatars}">
-      <h5 class="tweeterName">${object.user.name}</h5>
+      <img class="avatar" src="${tweet.user.avatars}">
+      <h5 class="tweeterName">${tweet.user.name}</h5>
     </div>
     <div class="tweetUser">
-      <h4 class="userName">${object.user.handle}</h4>
+      <h4 class="userName">${tweet.user.handle}</h4>
     </div>
   </header>
-    <article class="tweetText">${object.content.text}</article>
+    <article class="tweetText">${tweet.content.text}</article>
   <hr>
   <footer class="tweetFooter">
-    <h6>${object.created_at}</h6>
+    <h6>${tweet.created_at}</h6>
     <div>
       <i class="fa-solid fa-flag icons"></i>
       <i class="fa-sharp fa-solid fa-retweet icons"></i>
@@ -41,13 +104,19 @@ const createTweetElement = function(object) {
   </footer>
 </article>`;
 
-  return markup;
+  return $tweet;
 };
 
-const $tweet = createTweetElement(tweetData);
+const renderTweets = function(tweets) {
 
+  for (const tweet of tweets) {
 
+    const $tweet = createTweetElement(tweet);
 
-$(document).ready(function() {
-  $('#tweets-container').append($tweet);
-});
+    $(document).ready(function() {
+      $('#tweets-container').append($tweet);
+    });
+  }
+  return;
+};
+renderTweets(data);
