b5d897fb899f1c66c6f2346eff069a7f383e7dda
Thien Trieu <thien.trieu@telus.com>
2022-12-05 20:39:47 -0800

    implement counter with jquery and layout and css for new tweet

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+
+
+  $('#tweet-text').keydown(function() {
+    const tweetLength = $(this).val().length;
+    const counter = $(this).siblings().children()[1];
+
+    $(counter).text(140 - tweetLength);
+
+    if (tweetLength > 140) {
+      $(counter).css('color', 'red');
+    } else {
+      $(counter).css('color', '#545149');
+    }
+
+
+  });
+
+
+
+});
